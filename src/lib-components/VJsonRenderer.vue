2d6aef1d4ca137ed4de409314540f7fb9e917d20
dominikhorn93 <dominik.horn93@gmail.com>
2022-03-14 09:19:28 +0100

    fix: readonly mode

--- src/lib-components/VJsonRenderer.vue
+++ src/lib-components/VJsonRenderer.vue
@@ -66,9 +66,9 @@ export default class VJsonRenderer extends Vue {
 
 <style>
 
-.read-only .v-input--switch.v-input--is-dirty.v-input--is-disabled {
-  opacity: 1 !important;
-}
+/*.read-only .v-input--switch.v-input--is-dirty.v-input--is-disabled {*/
+/*  opacity: 1 !important;*/
+/*}*/
 
 .v-input--is-disabled:not(.v-input--is-readonly) a {
   pointer-events: all !important;
@@ -79,16 +79,16 @@ export default class VJsonRenderer extends Vue {
 }
 
 .read-only fieldset {
-  border: none !important;
-  padding-left: 0 !important;
+  background: rgb(245, 245, 245);
 }
 
-.read-only .v-input__slot {
-  padding-left: 0 !important;
-}
+/*.read-only .v-input__slot {*/
+/*  padding-left: 0 !important;*/
+/*}*/
 
 .read-only .v-label {
   color: #222 !important;
+  font-size: 16px !important;
 }
 
 .vjsf-property > .v-input--hide-details {
